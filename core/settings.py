ad937bc739b6b6782c94eddd5719f1f81d876f08
Dawit Nigus <101523835+Dawit-Nigus@users.noreply.github.com>
2022-09-07 19:23:48 +0300

    Host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qyu(9l9v%^+r(vt#ecf+36#lis516#3bo5@bo-rd*d%a=!%8#!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['stock-management-system-django.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
